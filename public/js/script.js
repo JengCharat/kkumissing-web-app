b1d494136458ce2163a0d5b9b81e268d597b79a4
แก้วเพ็ชรัตน์ สีสันต์ <jengcharat@Macintosh-8.local>
2025-02-02 08:41:04 +0700

    choose between daily monthly in html page

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,11 @@ function select_this_room(id) {
     document.getElementById("room_ID_select").innerHTML = id;
     document.getElementById("room_ID_select2").value = id;
 }
+function daily_form() {
+    document.getElementById("monthly_form").style.display = "none";
+    document.getElementById("daily_form").style.display = "block";
+}
+function monthly_form() {
+    document.getElementById("daily_form").style.display = "none";
+    document.getElementById("monthly_form").style.display = "block";
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,71 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-         <script src ="{{ asset('js/script.js') }}" > </script>
-    </head>
-    <body>
-       <h1>firstpage</h1>
-        <h2>left room</h2>
-        <table border="1">
-            <tbody>
-                @foreach ($rooms->chunk(6) as $room)
-                <tr>
-                    @foreach ($room as $item)
-                        @if ($item -> status == "Available")
-                        <td><button style="background: green;" type="button" onclick="select_this_room('{{ $item->roomNumber }}')">{{ $item->roomNumber }}</button></td>
-                        @else
-                        <td  style="background:red;">{{ $item->roomNumber }}</td>
-                        @endif
-                    @endforeach
-                </tr>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/script.js') }}"> </script>
+</head>
+
+<body>
+    <h1>firstpage</h1>
+    <h2>left room</h2>
+    <table border="1">
+        <tbody>
+            @foreach ($rooms->chunk(6) as $room)
+            <tr>
+                @foreach ($room as $item)
+                @if ($item -> status == "Available")
+                <td><button style="background: green;" type="button"
+                        onclick="select_this_room('{{ $item->roomNumber }}')">{{ $item->roomNumber }}</button></td>
+                @else
+                <td style="background:red;">{{ $item->roomNumber }}</td>
+                @endif
                 @endforeach
-           </tbody>
-        </table>
-
-        <h2>right room</h2>
-        <table id = "test">
-            <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
             </tr>
-            <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-            </tr>
-            </table>
+            @endforeach
+        </tbody>
+    </table>
+
+    <h2>right room</h2>
+    <table id="test">
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+    </table>
 
 
-           <form method="POST" action="/hire">
-               @csrf
-               <h1 id = "room_ID_select"></h1>
-                <input type="hidden" name="roomNumber" id = "room_ID_select2" value="">
-                <button type="button">BOOK NOW</button>
-                <br>
-                <button type="button">รายวัน</button>
-                <button type="button">รายเดือน</button>
+        <h1 id="room_ID_select"></h1>
+        <input type="hidden" name="roomNumber" id="room_ID_select2" value="">
+        <button type="button">BOOK NOW</button>
+        <br>
+        <button type="button" onclick="daily_form()">รายวัน</button>
+        <button type="button" onclick="monthly_form()">รายเดือน</button>
+
+        <div id = "monthly_form" class="monthly_form" style = "display:none">
+            <h1>monthly</h1>
+
+            <form method="POST" action="/hire">
+            @csrf
                 <h1>checkin</h1>
-                <input type="date" name = "checkin">
+                <input type="date" name="checkin">
                 <h1>checkout</h1>
-                <input type="date" name = "checkout">
+                <input type="date" name="checkout">
 
                 <h1>ชื่อผู้เช่า</h1>
-                <input type="text" name = "tenantName">
+                <input type="text" name="tenantName">
                 <h1>เบอร์</h1>
-                <input type="tel" name = "tenantTel">
+                <input type="tel" name="tenantTel">
                 <h1>payment</h1>
                 <button type="button">qr code</button>
                 <button type="button">เงินสด</button>
@@ -73,6 +80,34 @@
                 <input type="file">
                 <br>
                 <button type="submit">finish submit</button>
-           </form>
-    </body>
+
+            </form>
+        </div>
+
+
+        <div id = "daily_form" class="daily_form" style = "display:none">
+            <h1>daily</h1>
+            <form method="POST" action="/hire">
+            @csrf
+                <h1>checkin</h1>
+                <input type="date" name="checkin">
+                <h1>checkout</h1>
+                <input type="date" name="checkout">
+
+                <h1>ชื่อผู้เช่า</h1>
+                <input type="text" name="tenantName">
+                <h1>เบอร์</h1>
+                <input type="tel" name="tenantTel">
+                <h1>payment</h1>
+                <button type="button">qr code</button>
+                <button type="button">เงินสด</button>
+                <h1>upload slip</h1>
+                <input type="file">
+                <br>
+                <button type="submit">finish submit</button>
+
+            </form>
+        </div>
+</body>
+
 </html>
