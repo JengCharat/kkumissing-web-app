a441851701b849c636efe368a66e159cf44198a0
แก้วเพ็ชรัตน์ สีสันต์ <jengcharat@Macintosh-8.local>
2025-02-05 09:19:36 +0700

    คำนวณค่าน้ำค่าไฟ

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\Tenant;
 use App\Models\Room;
 use App\Models\MeterReading;
 use App\Models\MeterDetails;
+use App\Models\Expense;
 class DashboardController extends Controller
 {
     //
@@ -18,6 +19,11 @@ class DashboardController extends Controller
         $rooms = Room::where('roomnumber',$roomNumber)->first();
         $meter_reading = MeterReading::Where('room_id',$rooms->roomID)->first();
         $meter_detail = MeterDetails::Where('meter_detailID',$meter_reading->meter_details_id)->first();
+
+        $expense = Expense::Where('expenseID',1)->first();
+        $rooms->water_price =(($meter_detail->water_meter_end)-($meter_detail->water_meter_start)) * $expense->unit_price_water;
+        $rooms->electricity_price = (($meter_detail->electricity_meter_end) - ($meter_detail->electricity_meter_start)) * $expense->unit_price_electricity;
+        $rooms->save();
         return view('dashboard',compact('userId','rooms','meter_reading','meter_detail'));
     }
 }
