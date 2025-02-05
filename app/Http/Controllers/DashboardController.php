e4ed320c3fb4fdc824020621ad35935090ae0a4b
แก้วเพ็ชรัตน์ สีสันต์ <jengcharat@Macintosh-8.local>
2025-02-05 08:26:40 +0700

    show meter detail in dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tenant;
 use App\Models\Room;
+use App\Models\MeterReading;
+use App\Models\MeterDetails;
 class DashboardController extends Controller
 {
     //
@@ -14,6 +16,8 @@ class DashboardController extends Controller
         $roomNumber = Auth::user()->name;
 
         $rooms = Room::where('roomnumber',$roomNumber)->first();
-        return view('dashboard',compact('userId','rooms'));
+        $meter_reading = MeterReading::Where('room_id',$rooms->roomID)->first();
+        $meter_detail = MeterDetails::Where('meter_detailID',$meter_reading->meter_details_id)->first();
+        return view('dashboard',compact('userId','rooms','meter_reading','meter_detail'));
     }
 }
