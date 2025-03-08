604727c85b8ccf383f27717ce689a9d960a7598b
JengCharat <zazajeng401@gmail.com>
2025-03-08 10:06:18 +0700

    fix/diable_date

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -251,11 +251,11 @@
 
                             <div class="flex-1 min-w-[200px]">
                                 <label class="block text-sm font-medium text-gray-700 mb-1">เช็คอิน</label>
-                                <input type="date" name="checkin" value="{{ $check_in ?? '' }}" class="w-full px-3 py-2 border border-gray-300 rounded-md" required>
+                                <input disabled type="date" name="checkin" value="{{ $check_in ?? '' }}" class="w-full px-3 py-2 border border-gray-300 rounded-md" required>
                             </div>
                             <div class="flex-1 min-w-[200px]">
                                 <label class="block text-sm font-medium text-gray-700 mb-1">เช็คเอาท์</label>
-                                <input type="date" name="checkout" value="{{ $check_out ?? '' }}" class="w-full px-3 py-2 border border-gray-300 rounded-md" required>
+                                <input disabled type ="date" name="checkout" value="{{ $check_out ?? '' }}" class="w-full px-3 py-2 border border-gray-300 rounded-md" required>
                             </div>
 
                             <div>
